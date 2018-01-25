54d1450e6b17732a9f506ecd53cdadd820dc9a5e
misha <mischa.schwed@gmail.com>
2018-01-26 01:38:29 +0300

    new commit2

---
+++ src/main/java/by/tc/shved/task01/factory/ApplianceCreator.java
@@ -0,0 +1,10 @@
+package by.tc.shved.task01.factory;
+
+import by.tc.shved.task01.entity.Appliance;
+
+public class ApplianceCreator {
+
+    public Appliance create(String applianceTextFormat){
+        return null;
+    }
+}
