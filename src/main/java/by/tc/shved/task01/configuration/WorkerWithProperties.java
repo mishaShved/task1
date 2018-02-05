5853e1a16b8f8a1b0a96f3805a929fc274cd14c1
OSmol <Olga_Smolyakova@outlook.com>
2018-02-05 16:36:18 +0300

    Update WorkerWithProperties.java

--- src/main/java/by/tc/shved/task01/configuration/WorkerWithProperties.java
+++ src/main/java/by/tc/shved/task01/configuration/WorkerWithProperties.java
@@ -5,7 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-public class WorkerWithProperties {
+public class WorkerWithProperties {// и как я должна понять, где мне этот класс приенять, он настолько глобален?
 
     private static WorkerWithProperties ourInstance;
     private String fileName;
@@ -13,7 +13,8 @@ public class WorkerWithProperties {
     private WorkerWithProperties() throws IOException{
 
         Properties props = new Properties();
-        props.load(new FileInputStream(new File("src/main/resources/properties.ini")));
+        props.load(new FileInputStream(new File("src/main/resources/properties.ini")));// хардкодим путь к src...
+        // вот ты задумался, как твоя программа будет выполняться без IDE, ведь уже не будет src каталога
         fileName = props.getProperty("FILE_NAME");
 
     }
